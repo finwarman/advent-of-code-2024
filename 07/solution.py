67f585448baa8f62e7ec49834b6d9316668111ed
Fin <finleywarman@hotmail.co.uk>
2024-12-07 09:33:11 +0000

    Add multiprocessing for day 7 solution

--- 07/solution.py
+++ 07/solution.py
@@ -1,37 +1,39 @@
 #! /usr/bin/env python3
 
-with open('input.txt', 'r', encoding='ascii') as file:
-    DATA = file.read().rstrip()
+from multiprocessing import Pool
 
-ROWS = DATA.strip().splitlines()
-ROWS = [list(map(int, line.replace(':', '').split(' '))) for line in ROWS]
+with open('input.txt', 'r', encoding='ascii') as file:
+    DATA = file.read().strip()
+
+ROWS = [list(map(int, line.replace(':', '').split(' '))) for line in DATA.splitlines()]
+ROWS = [(line[0], line[1:]) for line in ROWS] # (total, operands)
 
 # evaluates left-to-right, checking each operator configuration
-def eval_branch(target, current_sum, operands, idx, allow_concat=False):
+def process_equation(target, current_sum, operands, idx, allow_concat=False):
     if current_sum > target:
         return 0
-    if idx >= len(operands) - 1 and current_sum != target:
-        return 0
-    if idx == len(operands) - 1 and current_sum == target:
-        return target
+    if idx >= len(operands) - 1:
+        return target if current_sum == target else 0
 
-    if eval_branch(target, current_sum + operands[idx+1], operands, idx + 1, allow_concat):
-        return target
-    elif eval_branch(target, current_sum * operands[idx+1], operands, idx + 1, allow_concat):
-        return target
-    elif allow_concat and eval_branch(target, int(str(current_sum) + str(operands[idx+1])), operands, idx + 1, allow_concat):
-        return target
-    return 0
+    result = 0
+    if process_equation(target, current_sum + operands[idx+1], operands, idx + 1, allow_concat):
+        result = target
+    elif process_equation(target, current_sum * operands[idx+1], operands, idx + 1, allow_concat):
+        result = target
+    elif allow_concat:
+        concat_value = int(str(current_sum) + str(operands[idx+1]))
+        result = process_equation(target, concat_value, operands, idx + 1, allow_concat)
+
+    return result
 
 def get_total(allow_concat=False):
-    total_valid = 0
+    with Pool() as pool:
+        return sum(pool.starmap(process_equation, [
+            (total, operands[0], operands, 0, allow_concat)
+            for total, operands in ROWS
+        ]))
 
-    for equation in ROWS:
-        total, operands = equation[0], equation[1:]
-        total_valid += eval_branch(total, operands[0], operands, 0, allow_concat)
-
-    return total_valid
-
-print(get_total()) # 1708857123053
-
-print(get_total(True)) # 189207836795655
+# entry point, to allow multiprocessing
+if __name__ == "__main__":
+    print(get_total())       # 1708857123053
+    print(get_total(True))   # 189207836795655
