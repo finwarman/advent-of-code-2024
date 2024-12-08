c74878de9ef1aa96c289ecd8dbb4c116a3070844
Fin <finleywarman@hotmail.co.uk>
2024-12-08 10:32:12 +0000

    Simplify collated method

--- 08/solution.py
+++ 08/solution.py
@@ -7,15 +7,15 @@ with open('input.txt', 'r', encoding='ascii') as file:
     DATA = file.read().strip()
 
 GRID = [list(line) for line in DATA.splitlines()]
-WIDTH, HEIGHT = len(GRID[0]), len(GRID)
+WIDTH, HEIGHT = len(GRID[0]), len(GRID) # square
 
 FREQS = defaultdict(list)
 
 for y in range(HEIGHT):
     for x in range(WIDTH):
-        f = GRID[y][x]
-        if f != '.':
-            FREQS[f].append((x, y)) # in order
+        freq = GRID[y][x]
+        if freq != '.':
+            FREQS[freq].append((x, y)) # in order
 
 def generate_antinodes(all_harmonics=False):
     antinodes = set()
@@ -24,35 +24,23 @@ def generate_antinodes(all_harmonics=False):
         for pos_a, pos_b in combinations(antennas, 2):
             dx, dy = (pos_b[0] - pos_a[0]), (pos_b[1] - pos_a[1])
 
+            start, limit = 1, 1 # first only
             if all_harmonics:
-                limit = min(WIDTH // abs(dx), HEIGHT // abs(dy))
+                start, limit = 0, (WIDTH // abs(dx))
 
-                for mult in range(limit):
-                    x, y = node = ((pos_a[0] - (mult*dx)), (pos_a[1] - (mult*dy)))
+            for mult in range(start, limit+1):
+                node = ((pos_a[0] - (mult*dx)), (pos_a[1] - (mult*dy)))
+                if 0 <= node[0] < WIDTH and 0 <= node[1] < HEIGHT:
+                    antinodes.add(node)
+                else:
+                    break
 
-                    if 0 <= x < WIDTH and 0 <= y < HEIGHT:
-                        antinodes.add(node)
-                    else:
-                        break
-
-                for mult in range(limit):
-                    x, y = node = ((pos_b[0] + (mult*dx)), (pos_b[1] + (mult*dy)))
-
-                    if 0 <= x < WIDTH and 0 <= y < HEIGHT:
-                        antinodes.add(node)
-                    else:
-                        break
-            else:
-                nodes =[
-                    ((pos_a[0] - dx), (pos_a[1] - dy)),
-                    ((pos_b[0] + dx), (pos_b[1] + dy)),
-                ]
-
-                for node in nodes:
-                    x, y = node
-                    if 0 <= x < WIDTH and 0 <= y < HEIGHT:
-                        antinodes.add(node)
-
+            for mult in range(start, limit+1):
+                node = ((pos_b[0] + (mult*dx)), (pos_b[1] + (mult*dy)))
+                if 0 <= node[0] < WIDTH and 0 <= node[1] < HEIGHT:
+                    antinodes.add(node)
+                else:
+                    break
     return len(antinodes)
 
 # part 1
