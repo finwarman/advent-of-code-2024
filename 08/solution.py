9d2b088c8d36e57d78b07f036109f35944bd9c85
Fin <finleywarman@hotmail.co.uk>
2024-12-08 10:16:33 +0000

    Collate into single method

--- 08/solution.py
+++ 08/solution.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python3
 
 from collections import defaultdict
+from itertools import combinations
 
 with open('input.txt', 'r', encoding='ascii') as file:
     DATA = file.read().strip()
@@ -8,86 +9,56 @@ with open('input.txt', 'r', encoding='ascii') as file:
 GRID = [list(line) for line in DATA.splitlines()]
 WIDTH, HEIGHT = len(GRID[0]), len(GRID)
 
-freqs = defaultdict(list)
+FREQS = defaultdict(list)
 
 for y in range(HEIGHT):
     for x in range(WIDTH):
         f = GRID[y][x]
         if f != '.':
-            freqs[f].append((x, y))
+            FREQS[f].append((x, y)) # in order
+
+def generate_antinodes(all_harmonics=False):
+    antinodes = set()
+    for antennas in FREQS.values():
+        # for each pair of antennas
+        for pos_a, pos_b in combinations(antennas, 2):
+            dx, dy = (pos_b[0] - pos_a[0]), (pos_b[1] - pos_a[1])
+
+            if all_harmonics:
+                limit = min(WIDTH // abs(dx), HEIGHT // abs(dy))
+
+                for mult in range(limit):
+                    x, y = node = ((pos_a[0] - (mult*dx)), (pos_a[1] - (mult*dy)))
+
+                    if 0 <= x < WIDTH and 0 <= y < HEIGHT:
+                        antinodes.add(node)
+                    else:
+                        break
+
+                for mult in range(limit):
+                    x, y = node = ((pos_b[0] + (mult*dx)), (pos_b[1] + (mult*dy)))
+
+                    if 0 <= x < WIDTH and 0 <= y < HEIGHT:
+                        antinodes.add(node)
+                    else:
+                        break
+            else:
+                nodes =[
+                    ((pos_a[0] - dx), (pos_a[1] - dy)),
+                    ((pos_b[0] + dx), (pos_b[1] + dy)),
+                ]
+
+                for node in nodes:
+                    x, y = node
+                    if 0 <= x < WIDTH and 0 <= y < HEIGHT:
+                        antinodes.add(node)
+
+    return len(antinodes)
 
 # part 1
 
-antinodes = set()
-
-for freq in freqs:
-    antennas = list(freqs[freq])
-
-    # for each pair of antennas
-    for i, ant in enumerate(antennas):
-        for j in range(i+1, len(antennas)):
-            pos_a, pos_b = antennas[i], antennas[j]
-
-            dx, dy = (pos_b[0] - pos_a[0]), (pos_b[1] - pos_a[1])
-            nodes =[
-                ((pos_a[0] - dx), (pos_a[1] - dy)),
-                ((pos_b[0] + dx), (pos_b[1] + dy)),
-            ]
-
-            for node in nodes:
-                x, y = node
-                if 0 <= x < WIDTH and 0 <= y < HEIGHT:
-                    antinodes.add(node)
-
-print(len(antinodes)) # 280
+print(generate_antinodes()) # 280
 
 # part 2
 
-antinodes = set()
-
-for freq in freqs:
-    antennas = list(freqs[freq])
-
-    # for each pair of antennas
-    for i, ant in enumerate(antennas):
-        for j in range(i+1, len(antennas)):
-            pos_a, pos_b = antennas[i], antennas[j]
-
-            dx, dy = (pos_b[0] - pos_a[0]), (pos_b[1] - pos_a[1])
-            nodes =[
-                ((pos_a[0] - dx), (pos_a[1] - dy)),
-                ((pos_b[0] + dx), (pos_b[1] + dy)),
-            ]
-
-            for node in nodes:
-                x, y = node
-                if 0 <= x < WIDTH and 0 <= y < HEIGHT:
-                    antinodes.add(node)
-
-
-            dx, dy = (pos_b[0] - pos_a[0]), (pos_b[1] - pos_a[1])
-
-            for mult in range(100):
-                x, y = node = ((pos_a[0] - (mult*dx)), (pos_a[1] - (mult*dy)))
-
-                if 0 <= x < WIDTH and 0 <= y < HEIGHT:
-                    antinodes.add(node)
-                else:
-                    break
-
-            for mult in range(100):
-                x, y = node = ((pos_b[0] + (mult*dx)), (pos_b[1] + (mult*dy)))
-
-                if 0 <= x < WIDTH and 0 <= y < HEIGHT:
-                    antinodes.add(node)
-                else:
-                    break
-
-# for node in antinodes:
-#     x, y = node
-#     GRID[y][x] = '#'
-
-# for row in GRID:
-#     print(''.join(row))
-
-print(len(antinodes)) # 958
+print(generate_antinodes(all_harmonics=True)) # 958
